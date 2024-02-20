d90675b76cd6252b9a6965fdae1ec1268712d032
DonLemos <donovangarethlemos@gmail.com>
2024-02-20 21:12:17 +0200

    Update

--- Node.js/get-programming-with-nodejs/get-programming-with-nodejs/unit_3/lesson_13/start/recipe_app/main.js
+++ Node.js/get-programming-with-nodejs/get-programming-with-nodejs/unit_3/lesson_13/start/recipe_app/main.js
@@ -1,10 +1,28 @@
 "use strict";
 
+
+
 const express = require("express"),
   app = express(),
   errorController = require("./controllers/errorController"),
   homeController = require("./controllers/homeController"),
   layouts = require("express-ejs-layouts");
+
+const MongoDB = require("mongodb").MongoClient,
+  dbURL = "mongodb://localhost:27017",
+  dbName = "recipe_db";
+
+MongoDB.connect(dbURL, (error, client) => {
+  if (error) throw error;
+
+  let db = client.db(dbName);
+  db.collection("contacts")
+    .find()
+    .toArray((error, data) => {
+      if (error) throw error;
+      console.log(data);
+    });
+});
 
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
@@ -35,3 +53,4 @@ app.use(errorController.respondInternalError);
 app.listen(app.get("port"), () => {
   console.log(`Server running at http://localhost:${app.get("port")}`);
 });
+
